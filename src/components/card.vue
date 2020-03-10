e4eab4fa88f1ea3fdbf4bced68bb375d4ee6b4c4
g7tro4w <g7tro4w@gmail.com>
2020-03-10 23:34:48 +0300

    Добавлен storybook для компонентов, улучшен компонент card

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,22 +1,45 @@
 <template>
-  <div class="v-card">
+  <div
+    :class="[
+        'vish-card',
+        {'vish-card_flat': flat},
+        {'vish-card_full-width': fullWidth}
+    ]"
+  >
       <slot></slot>
   </div>
 </template>
 <script>
 export default {
-    name: 'v-card'
+    name: 'v-card',
+    props: {
+        flat: {
+            type: Boolean,
+            default: false
+        },
+        fullWidth: {
+            type: Boolean,
+            default: false
+        }
+    }
 }
 </script>
 <style>
-  .v-card {
+  .vish-card {
     background: #FFFFFF;
     box-shadow: 0px 1px 2px rgba(9, 26, 64, 0.23);
     border-radius: 16px;
     padding: 28px 24px;
-    display: inline-flex;
+    display: inline-block;
     flex-direction: row;
     justify-content: flex-start;
     align-items: stretch;
+  }
+  .vish-card_flat {
+      box-shadow: none;
+  }
+  .vish-card_full-width {
+      width: 100%;
+      display: block;
   }
 </style>
