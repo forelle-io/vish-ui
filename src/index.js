194226b0990a49f8b8e6beef4a7a2b1583824e80
g7tro4w <g7tro4w@gmail.com>
2020-03-17 00:42:47 +0300

    Добавлены layout-строки, обновлена дока

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import UIButton from './components/button.vue'
 import UICard from './components/card.vue'
 import UIInput from './components/input.vue'
+import UIRow from './components/row.vue'
 
 export default {
     install (Vue) {
         Vue.component('vish-card', UICard)
         Vue.component('vish-button', UIButton)
         Vue.component('vish-input', UIInput)
+        Vue.component('vish-row', UIRow)
     }
 }
